7c5cc42d972da887ddce88725b8274308a3745ba
farr311 <40473601+farr311@users.noreply.github.com>
2022-11-08 13:44:49 +0200

    Recursion lesson

---
+++ src/lesson3/Recursion.java
@@ -0,0 +1,43 @@
+package lesson3;
+
+public class Recursion {
+
+    public static void main(String[] args) {
+
+        /*for (int i = 1; i < 10; i++) {
+            System.out.println(fibNonRec(i));
+        }*/
+
+        double start = System.nanoTime();
+       // System.out.println(fibRec(50));
+        System.out.println(fibNonRec(1000));
+
+        System.out.println((System.nanoTime() - start) / 1_000_000_000);
+    }
+
+    public static long fibRec(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+
+        return fibRec(n - 1) + fibRec(n - 2);
+    }
+
+    public static long fibNonRec(int n) {
+        long a = 1;
+        long b = 1;
+        long result = 1;
+
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+
+        for (int i = 2; i < n; i++) {
+            result = a + b;
+            a = b;
+            b = result;
+        }
+
+        return result;
+    }
+}
