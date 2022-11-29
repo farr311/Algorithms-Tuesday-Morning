cc3a021044e9fee90f73911a931ae5362586d5ee
farr311 <40473601+farr311@users.noreply.github.com>
2022-11-29 13:16:39 +0200

    BubbleSort

---
+++ src/lesson6/BubbleSort.java
@@ -0,0 +1,19 @@
+package lesson6;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+
+    }
+
+    static void bubbleSort(int[] arr) {
+        // Написать условия циклов
+        // Выполнить сравнение каждого элемента с каждым
+        // Если текущий элемент больше следующего, поменять их местами
+        for () {
+            for () {
+                // swap elements if first > second
+            }
+        }
+    }
+}
